ad55946f04cae9c7bdf59bfde202f89e4263b86c
hcw0915 <love233031@gmail.com>
2022-11-23 08:55:12 +0800

    1123 update

--- app01/urls.py
+++ app01/urls.py
@@ -3,9 +3,16 @@ from .views import index, listing, posting, contact, post2db
 
 urlpatterns = [
   path('', index),
+  
+  # 這是給刪除功能
   path('<int:pid>/<str:del_pass>', index),
-  path('list/', listing),
-  path('post/', posting),
-  path('contact/', contact),
-  path('post2db/', post2db),
+
+  # 實際只是把 index 裡的功能分離出來 => 發布貼文、列表、
+  # path('list/', listing),
+  # path('post/', posting),
+  # path('post2db/', post2db),
+
+  # 恩?? 好像沒這個東西
+  # path('contact/', contact),
+
 ]
--- app01/views.py
+++ app01/views.py
@@ -4,13 +4,17 @@ from .forms import ContactForm, PostForm
 
 # Create your views here.
 
-# 確認 pid=None, del_pass=None 是否為預設意義 => 若無傳入則為 None
+# pid=None, del_pass=None 針對兩變數實施"預設值設定"避免報錯 => 若無傳入則為 None
 def index(request, pid=None, del_pass=None): 
+
+  # 這裡跟 listing 一樣功能 
   # 篩選 Post裡 啟用(enabled=True)，然後以建立時間(-pub_time)進行降冪排列(-order_by)，[:30]表示顯示 前30筆資料 (0-29)
+  # 指定 moods 為物件 => <QuerySet [<Mood: 開心>, <Mood: 難過>]>
   posts = Post.objects.filter(enabled=True).order_by('-pub_time')[:30] 
-  #? 指定 moods 為物件 => <QuerySet [<Mood: 開心>, <Mood: 難過>]>
   moods = Mood.objects.all()
-  try: # 從 form(method='GET')表單
+  
+  ''' 這一串可以往下移到 Create 的地方
+  try: # 從 form ( method='GET')表單中拿取提交後資料(名稱連接 form裡的 input的 name),
     user_id = request.GET.get('user_id')
     user_pass = request.GET.get('user_pass')
     user_post = request.GET.get('user_post')
@@ -18,10 +22,11 @@ def index(request, pid=None, del_pass=None):
   except:
     user_id = None
     message = '如果要張貼訊息則每一個欄位都要填...'
-
-  # TODO 此欄位為 DELETE- JavaScript做導向，導到 index做驗證
+  '''
+  # TODO 此欄位為 DELETE - 刪除貼文 ( JavaScript做 刪除鈕按鍵導向，導到 index這裡做刪除驗證 )
   # 此地方的值來自於 base.html JavaScript的 var usr = '/' + id + '/' + user_pass 傳值到 urls.
-  # 因為是共用同一個 views.index 所以一旦 urls 有值 del_pass, pid 則會執行刪除段 
+  # 因為是共用同一個 views.index 所以一旦 del_pass, pid 有值，就會傳入 urls 執行 刪除段，沒有值就會傳入預設值 None
+  # 然後直接不執行 if del_pass and pid ( None 屬於 False )，直接跳到 posting功能
   if del_pass and pid :
     try:
       # 確認密碼及ID後則先找到對應的 pid物件
@@ -32,16 +37,26 @@ def index(request, pid=None, del_pass=None):
     # 如果 post 確認有值(非 None)則為 True
     if post:
       # 張貼/刪除碼 在這裡驗證(validation)
+      # 確認填寫的刪除碼跟資料庫的一樣就過
       if post.del_pass == del_pass:
         post.delete()
         message = '資料刪除成功'
       else:
         message = '密碼錯誤'
 
-  # TODO 此欄位為 CREATE，但使用者確認沒有輸入
+  # TODO 此欄位為 CREATE - 建立貼文後，資料裡 enable = False => 要管理員手動開啟後 改成 True => 所以提示才會說需要管理員審核
+  # 這裡跟 posting 一樣功能 
   # 如果 暱稱 user_id有值(不等於none)，則建立一個新的資料
+  try: # 從 form ( method='GET')表單中拿取提交後資料(名稱連接 form裡的 input的 name),
+    user_id = request.GET.get('user_id')
+    user_pass = request.GET.get('user_pass')
+    user_post = request.GET.get('user_post')
+    user_mood = request.GET.get('mood')
+  except:
+    user_id = None
+    message = '如果要張貼訊息則每一個欄位都要填...'
+
   if user_id != None:
-    print(user_mood)
     mood = Mood.objects.get(status=user_mood)
     post = Post.objects.create(
       mood=mood,
@@ -51,14 +66,19 @@ def index(request, pid=None, del_pass=None):
       )
     post.save()
     message='成功儲存!請記得你的密碼[{}]!，訊息需要經過審查之後才會顯示。'.format(user_pass)
-
   return render(request, 'index.html', locals())
 
+
+
+# ==================================================== 以下都沒用到
+
+# 所以這個沒用到
 def listing(request):
   posts = Post.objects.all().order_by('-pub_time')[:150]
   moods = Mood.objects.all()
   return render(request, 'listing.html', locals())
 
+# 所以這個沒用到 (內容跟 index裡面的一樣)
 def posting(request):
   moods = Mood.objects.all()
   try:
@@ -83,6 +103,8 @@ def posting(request):
 
   return render(request, 'posting.html', locals())
 
+
+# 這個根本沒建立 ContactForm
 def contact(request):
   if request.method == 'POST':
     form = ContactForm(data = request.POST)
@@ -96,6 +118,7 @@ def contact(request):
       form = ContactForm()
     return render(request, 'contact.html', locals())
 
+# 這個沒用到(只是把 request.method改成POST => 這裡要用 POST，那 html裡的 form裡的 method也要改)
 def post2db(request):
   if request.method == 'POST':
     post_form = PostForm(data = request.POST)
